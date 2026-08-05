1f2bc84b28e4ea93931448440547fa58784474b1
RADIOS UNIDAS <josecutirehuayta@gmail.com>
2026-08-04 20:22:27 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,34 @@
-﻿const CACHE_NAME = 'radios-unidas-v1';
-self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+﻿// Service Worker - Radios Unidas | Notificaciones y PWA
+const CACHE_NOMBRE = 'radio-v2'; // actualiza v2 si cambias archivos luego
+
+// 📤 AL TOCAR LA NOTIFICACIÓN: ABRE O TRAE LA RADIO
+self.addEventListener('notificationclick', evento => {
+  evento.notification.close();
+
+  evento.waitUntil(
+    clients.matchAll({type: 'window'}).then( listaVentanas => {
+      for (const ventana of listaVentanas) {
+        if (ventana.url.includes('radiosunidas.github.io') && 'focus' in ventana) {
+          return ventana.focus();
+        }
+      }
+      if (clients.openWindow) return clients.openWindow('https://radiosunidas.github.io/eco/');
+    })
+  );
+});
+
+// GUARDAR ARCHIVOS PARA FUNCIONAMIENTO COMO APP
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NOMBRE).then(cache => cache.addAll([
+      './', './index.html', './manifest.json', './locutores.html',
+      'https://radiosunidas.github.io/SANCAMILO/imagen/logo.png'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(respuesta => respuesta || fetch(e.request))
+  );
+});
